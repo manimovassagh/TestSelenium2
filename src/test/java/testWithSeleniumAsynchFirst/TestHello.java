c7013559423070f619afc00fe7788cdf932df9ef
manimovassagh <mani.mgh@gmail.com>
2022-02-21 12:48:34 +0100

    added some more test

--- src/test/java/testWithSeleniumAsynchFirst/TestHello.java
+++ src/test/java/testWithSeleniumAsynchFirst/TestHello.java
@@ -1,12 +1,14 @@
 package testWithSeleniumAsynchFirst;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestHello {
@@ -18,9 +20,28 @@ public class TestHello {
 		System.out.println("Start The TEst Automation");
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://codingsolo.de");
+//		driver.get("https://codingsolo.de");
 
 	}
+
+//	@Test
+//	public void testTitle() {
+//		try {
+//			Thread.sleep(6000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		String expect = "coding blog - Coding Solo | programmierblog | tutorials | Videos";
+//
+//		driver.findElement(By.cssSelector("#header_main > div > div > span > a > img")).click();
+//
+//		driver.findElement(By.cssSelector("#cookie_action_close_header")).click();
+//		assertEquals(expect, driver.getTitle());
+//		assertEquals(expect, "coding blog - Coding Solo | programmierblog | tutorials | Videos");
+//		
+//		
+//	}
 
 	@After
 	public void afterTests() {
@@ -28,20 +49,39 @@ public class TestHello {
 		driver.close();
 	}
 
+//	@Test
+//	public void openCoingecko() {
+//		driver.get("https://www.coingecko.com/");
+//
+//		driver.findElement(By.cssSelector("#cookie-notice > div > div > div > div > button")).click();
+//		try {
+//			Thread.sleep(4000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		String selectorForSomeElement = "body > div.container > div:nth-child(3) > div:nth-child(1) > div > div > nav > a:nth-child(3)";
+//		driver.findElement(By.cssSelector(selectorForSomeElement)).click();
+//		try {
+//			Thread.sleep(4000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//	}
+
 	@Test
-	public void testTitle() {
-		try {
-			Thread.sleep(6000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String expect = "coding blog - Coding Solo | programmierblog | tutorials | Videos2";
+	public void checkSomeValuesForTodayAndPrintIt() {
+		driver.get("https://www.coingecko.com/");
+		String btcSelector="body > div.container > div.gecko-table-container > div.coingecko-table > div.position-relative > div > table > tbody > tr:nth-child(1) > td.td-price.price.text-right.pl-0";
+		WebElement btc= driver.findElement(By.cssSelector(btcSelector)); 
+		String btcPrice=btc.getAttribute("data-sort");
+		assertNotEquals(btcPrice, "38302.*");
+		
+		
+	
 
-		driver.findElement(By.cssSelector("#header_main > div > div > span > a > img")).click();
-
-		driver.findElement(By.cssSelector("#cookie_action_close_header")).click();
-		assertEquals(expect, driver.getTitle());
-		assertEquals(expect, "coding blog - Coding Solo | programmierblog | tutorials | Videos");
 	}
+
 }
