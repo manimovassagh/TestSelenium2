98d1ff28304ea44821515f9a73b12410827f714d
manimovassagh <mani.mgh@gmail.com>
2022-02-21 14:03:40 +0100

    2

--- src/test/java/testWithSeleniumAsynchFirst/TestHello.java
+++ src/test/java/testWithSeleniumAsynchFirst/TestHello.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,16 +14,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
 
 public class TestHello {
 	WebDriver driver;
+	
+	@FindBy(linkText = "Ethereum")
+	private WebElement etherium;
 
+	@SuppressWarnings("deprecation")
 	@Before
 	public void beforeTest() {
 
 		System.out.println("Start The TEst Automation");
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 //		driver.get("https://codingsolo.de");
 
 	}
@@ -75,14 +89,21 @@ public class TestHello {
 	@Test
 	public void checkSomeValuesForTodayAndPrintIt() {
 		driver.get("https://www.coingecko.com/");
-		String btcSelector="body > div.container > div.gecko-table-container > div.coingecko-table > div.position-relative > div > table > tbody > tr:nth-child(1) > td.td-price.price.text-right.pl-0";
-		WebElement btc= driver.findElement(By.cssSelector(btcSelector)); 
-		String btcPrice=btc.getAttribute("data-sort");
+		String btcSelector = "body > div.container > div.gecko-table-container > div.coingecko-table > div.position-relative > div > table > tbody > tr:nth-child(1) > td.td-price.price.text-right.pl-0";
+		WebElement btc = driver.findElement(By.cssSelector(btcSelector));
+		String btcPrice = btc.getAttribute("data-sort");
 		assertNotEquals(btcPrice, "38302.*");
 		assertTrue(btcPrice.contains("37"));
-		
-	
 
 	}
 
+	@Test
+	public void justCheck() {
+
+	System.out.println("Added Auto generated methods");
+	String text = etherium.getText();
+	
+	assertEquals(text, "Ethereum");
+	
+	}
 }
